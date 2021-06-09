3fa500045ca9d1db33232b48f3ed89ab22a37a74
Pierre Manchon <pierre.manchon@pm.me>
2021-06-09 10:40:51 +0200

    Deleted the get_pixel_diversity function because it is now used directly to avoid perfomance loss

--- PAIA/processing.py
+++ PAIA/processing.py
@@ -2,7 +2,6 @@
 import numpy as np
 import pandas as pd
 from alive_progress import alive_bar
-from numpy import ndarray
 from pandas import DataFrame
 from geopandas import GeoDataFrame
 from typing import AnyStr, SupportsInt
@@ -106,49 +105,6 @@ def get_anopheles_data(geodataframe_aoi: GeoDataFrame, i: SupportsInt, anopheles
     return geodataframe_aoi
 
 
-def get_pixel_diversity(dataset_path: AnyStr, band: SupportsInt) -> DataFrame:
-    """
-    As an input, i take the dataset path and the band number i want to categorize.
-    Then i export the counted values (from the __count_values() function).
-    To do so, i retrieve the path where the dataset is stored then join it to nameofthedataset_report.txt.
-    That way, i can save the report at the same place where the dataset is stored.
-    After that, i create the file and populate it with the field names then the values with formatted strings:
-        - Number of the category
-        - Number of pixels found
-        - Surface (based on the pixels surface (h*w) and the number of pixels)
-
-    :param dataset_path: The path to the dataset file
-    :type dataset_path: AnyStr
-    :param band: Specific band from a raster file
-    :type band: ndarray
-    :return: Export the counter to a text file row by row
-    :rtype: None
-    """
-    __dataset, __ctr = get_pixel_count(dataset_path=dataset_path, band=band)
-
-    data = []
-    for c in __ctr:
-        category_area = round(__ctr[c] * (__dataset.res[0] * __dataset.res[1]), 3)
-        raster_area = sum(__ctr.values())
-        percentage = ((__ctr[c] * 100) / raster_area)
-        data.append([c, __ctr[c], category_area, percentage])
-
-    df = pd.DataFrame(data, columns=['Category', 'Nbr of pixels', 'Surface (m2)', 'Proportion (%)'])
-
-    # TODO export style files from cropped raster so it can be read flawlessly here. Right now i have to load it into
-    #  qgis export it into a qml file by hand.
-    __dataset_name, _, __qml_path = format_dataset_output(dataset=dataset_path, ext='.qml')
-    __style = read_qml(__qml_path)
-    __val = None
-    for i, row in df.iterrows():
-        for j in __style:
-            if row['Category'] == j[0]:
-                __val = j[1]
-        df.loc[i, 'Label'] = __val
-
-    return df
-
-
 def get_pas_profiles(
         geodataframe_aoi: GeoDataFrame,
         aoi: AnyStr,
@@ -187,7 +143,7 @@ def get_pas_profiles(
     geodataframe_aoi.insert(geodataframe_aoi.shape[1] - 1, 'HABITAT_DIVERSITY', np.nan)
     # At the end but before the geometry column because the types of habitats come after it.
 
-    with alive_bar(total=len(geodataframe_aoi)*1) as bar_process:
+    with alive_bar(total=len(geodataframe_aoi)*3) as bar_process:
         # len(geodataframe_aoi*5 = Number of countries times the number of operations i need to do per countries
         for i, p in iter_poly(shapefile=geodataframe_aoi):
             # Retrieve the temporary file of the polygons.
@@ -223,8 +179,9 @@ def get_pas_profiles(
                 df_habitat_diversity.loc[i, 'Label'] = __val
             df_habitat_diversity = df_habitat_diversity.pivot_table(columns='Label',
                                                                     values='Proportion (%)',
-                                                                    aggfunc='sum')
-            df_habitat_diversity.name = i
+                                                                    aggfunc='sum').reset_index(drop=True)
+            df_habitat_diversity.reindex([i])
+            print(df_habitat_diversity.head())
             geodataframe_aoi = geodataframe_aoi.append(df_habitat_diversity)
             bar_process()  # Progress bar
             """
