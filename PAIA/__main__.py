debb3b83373dd94ecf2b259d15089b40ec3ecff1
Pierre Manchon <pierre.manchon@pm.me>
2021-06-10 15:37:46 +0200

    LICENSE
    Updated files for a proper License use

--- PAIA/__main__.py
+++ PAIA/__main__.py
@@ -1,5 +1,23 @@
 # -*-coding: utf8 -*
 """
+PAIA
+A tool to process data to learn more about Protected Areas Impact on Malaria Transmission
+
+Copyright (C) <2021>  <Manchon Pierre>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 Shapely (GEOS)
 Fiona (GDAL)
 pyproj (PROJ)
@@ -20,6 +38,11 @@ Pygis
 
 sentinelsat
 rasterio
+
+PAIA Copyright (C) 2021 Manchon Pierre
+This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+This is free software, and you are welcome to redistribute it
+under certain conditions; type `show c' for details.
 """
 from os import path, system, name
 from sys import argv, stderr, exit
