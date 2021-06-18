07fdcbe71b442b64d48be6f88ef1aabed59ab33c
Pierre Manchon <pierre.manchon@pm.me>
2021-06-18 02:45:09 +0200

    Moved and updated setup classifiers and requires

--- setup.py
+++ setup.py
@@ -35,6 +35,7 @@ entry_point = ''.join([cfgparser.get('setup', 'name'), ' = ', cfgparser.get('set
 setup(
     name=cfgparser.get('setup', 'name'),
     version=cfgparser.get('setup', 'version'),
+    license=cfgparser.get('setup', 'license'),
 
     author=cfgparser.get('setup', 'author'),
     author_email=cfgparser.get('setup', 'author_email'),
@@ -44,15 +45,53 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
 
+    classifiers=[
+        # How mature is this project? Common values are
+        #   1 - Planning
+        #   2 - Pre-Alpha
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        #   6 - Mature
+        #   7 - Inactive
+        'Development Status :: 5 - Production/Stable',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Information Technology',
+        "Topic :: Scientific/Engineering :: GIS",
+        "Topic :: Scientific/Engineering :: Visualization",
+        'Topic :: Scientific/Engineering :: Information Analysis',
+
+        'Natural Language :: English',
+        'Environment :: Console',
+        "Operating System :: OS Independent",
+
+        # Pick your license as you wish (should match "license" above)
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        "Programming Language :: Python :: 3.9"],
+
+    keywords=["gis",
+              "gis-utils",
+              "tools"],
+
     # https://stackoverflow.com/a/26082635
     entry_points={'console_scripts': [
         entry_point,
     ]},
 
-    install_requires=requirements,
-    dependency_links=[''.join(['file:\\', path.join(getcwd(), 'dependencies')])],
     packages=find_packages(),
     requirements=requirements,
+    install_requires=requirements,
+    python_requires=cfgparser.get('setup', 'python_requires'),
+    # https://stackoverflow.com/questions/55208309/installing-data-files-in-setup-py-with-pip-install-e
+    # data_files=[('', ['LICENSE'])],
+    dependency_links=[''.join(['file:\\', path.join(getcwd(), 'dependencies')])],
 )
+
 # # TODO Installer les dependencies de GDAL
 # https://stackoverflow.com/a/45262430
