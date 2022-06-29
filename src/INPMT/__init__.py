1a3a942ae20b1a5e7dfe35e5620d0e34a2268d28
Pierre Manchon <pierre.manchon@pm.me>
2022-06-29 23:44:30 +0200

    FIX: Typos

--- src/INPMT/__init__.py
+++ src/INPMT/__init__.py
@@ -18,8 +18,6 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-from configparser import ConfigParser as __ConfigParser
-
 # Built-ins
 import os
 import sys
