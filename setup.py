b4a4be34001af1e1b4daae6bc01ecdbf7f718cdf
Pierre Manchon <pierre.manchon@pm.me>
2022-06-30 20:34:45 +0200

    ADD: Allow and disallow execution of non signed scripts

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import subprocess
 import setuptools
 print('Installing dependencies...')
+# https://stackoverflow.com/a/4038991
+subprocess.run(['powershell.exe', 'Set-ExecutionPolicy RemoteSigned -Scope CurrentUser'])
 subprocess.run(['powershell.exe', 'dependencies/install.ps1'])
+subprocess.run(['powershell.exe', 'Set-ExecutionPolicy Restricted'])
 print('Done')
 setuptools.setup()
