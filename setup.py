75877c2f0f6a421eb995aa6760bfdb47d1691f8c
Pierre Manchon <pierre.manchon@pm.me>
2022-07-03 22:39:02 +0200

    ADD: Shell script for installing wheel on linux OSs

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+import sys
 import subprocess
 import setuptools
-print('Installing dependencies...')
 # https://stackoverflow.com/a/4038991
-subprocess.run(['powershell.exe', 'Set-ExecutionPolicy RemoteSigned -Scope CurrentUser'])
-subprocess.run(['powershell.exe', 'dependencies/install.ps1'])
-subprocess.run(['powershell.exe', 'Set-ExecutionPolicy Restricted'])
-print('Done')
+if sys.platform == 'win32' or sys.platform == 'win64':
+	subprocess.run(['powershell.exe', 'Set-ExecutionPolicy RemoteSigned -Scope CurrentUser'])
+	subprocess.run(['powershell.exe', 'dependencies/install.ps1'])
+	subprocess.run(['powershell.exe', 'Set-ExecutionPolicy Restricted'])
+else:
+	subprocess.run('dependencies/install.sh', shell=True)
 setuptools.setup()
