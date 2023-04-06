cd6dd6b8d6cb768121946bc9f374e79c4e0e7834
Pierre Manchon <pierre.manchon@pm.me>
2023-04-07 00:19:54 +0200

    DEL: Remove function get_countries_profile

--- INPMT/__main__.py
+++ INPMT/__main__.py
@@ -19,32 +19,25 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 import os
 import warnings
-from tempfile import TemporaryDirectory
-from typing import AnyStr
 
 import rioxarray as rxr
+from pandas import DataFrame
 
 try:
-    from __processing import get_countries_profile, get_urban_profile
+    from __processing import get_urban_profile
     from utils.utils import (
-        format_dataset_output,
         get_cfg_val,
     )
 except ImportError:
-    from INPMT.__processing import get_countries_profile, get_urban_profile
+    from INPMT.__processing import get_urban_profile
     from INPMT.utils.utils import (
-        format_dataset_output,
         get_cfg_val,
     )
 
 warnings.filterwarnings("ignore")
 
 
-def run(
-    method: AnyStr = ("villages", "countries"),
-    export_dir: AnyStr = "results",
-    loc: bool = True,
-) -> None:
+def run() -> DataFrame:
     """
     Retrieves the datasets path and executes the functions.
     For the countries, i only execute it like that.
@@ -52,21 +45,10 @@ def run(
         500, then i execute it after setting the
     parameter to 2000. Then i merge the two results.
 
-    :param loc:
-    :param method: Execute whether by villages or countries
-    :type method: AnyStr
-    :param export_dir:
-    :type export_dir: AnyStr
     :return: Nothing
     :rtype: None
     """
-    valid_method = ["countries", "villages"]
-    if method not in valid_method:
-        raise ValueError(
-            f"Invalid method parameter. Expected one of {valid_method}"
-        )
     datasets = get_cfg_val("datasets_storage_path")
-    export = os.path.join(datasets, export_dir)
 
     # Convert all raster data as xarray DataArrays
     population = rxr.open_rasterio(os.path.join(datasets, "POPULATION_AFRICA_100m_reprj3857.tif"))
@@ -85,42 +67,20 @@ def run(
     prevalence.name = 'prevalence'
 
     # Convert all vector data as a WKT geometry
-    irish = os.path.join(datasets, "IRISH_countries.shp")
-    landuse_polygonized = os.path.join(datasets, "LANDUSE_ESACCI-LC-L4-LC10-Map-300m-P1Y-2016-v1.0.shp")
+    os.path.join(datasets, "IRISH_countries.shp")
+    os.path.join(datasets, "LANDUSE_ESACCI-LC-L4-LC10-Map-300m-P1Y-2016-v1.0.shp")
     anopheles_kyalo_in_PAs_buffers = os.path.join(datasets, "KYALO_anopheles_in_PAs_buffers.shp")  # noqa F841
     national_parks_with_anopheles_kyalo = os.path.join(datasets, "NATIONAL_PARKS_WDPA_Africa_anopheles.shp")
     anopheles_kyalo = os.path.join(datasets, "KYALO_anopheles.shp")
 
-    with TemporaryDirectory(prefix="INPMT_") as tmp_directory:
-        # Read file as a geodataframe
-        if method == "countries":
-            gdf_profiles_aoi, path_profiles_aoi = get_countries_profile(
-                aoi=irish,
-                landuse=landuse,
-                landuse_polygonized=landuse_polygonized,
-                anopheles=anopheles_kyalo,
-                processing_directory=tmp_directory,
-            )
-            # Retrieves the directory the dataset is in and joins it the output
-            # filename
-            _, _, output_profiles = format_dataset_output(
-                dataset=export_dir, name="countries_profiles"
-            )
-            gdf_profiles_aoi.to_file(output_profiles)
-        if method == "villages":
-            profile_villages = get_urban_profile(
-                villages=anopheles_kyalo,
-                parks=national_parks_with_anopheles_kyalo,
-                population=population,
-                landuse=landuse,
-                ndvi=ndvi,
-                swi=swi,
-                gws=gws,
-                prevalence=prevalence)
-            # Retrieves the directory the dataset is in and joins it the output
-            # filename
-            _, _, output_vilages_profiles = format_dataset_output(
-                dataset=export, name="village_profiles", ext=".xlsx"
-            )
-            profile_villages.to_excel(output_vilages_profiles)
-            print("Jesus was black.")
+    profile_villages = get_urban_profile(
+        villages=anopheles_kyalo,
+        parks=national_parks_with_anopheles_kyalo,
+        population=population,
+        landuse=landuse,
+        ndvi=ndvi,
+        swi=swi,
+        gws=gws,
+        prevalence=prevalence)
+    print("Jesus was black.")
+    return profile_villages
